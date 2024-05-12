3ce2a1d873e52cbfdc5508096575bc26b3c277cc
minhDuc107886 <minhduc107886@gmail.com>
2024-05-12 23:13:09 +0700

    add information

--- lib/learn_dart/thamsotuychon_giatrimacdinh.dart
+++ lib/learn_dart/thamsotuychon_giatrimacdinh.dart
@@ -3,11 +3,12 @@ void main() {
   print_madinh();
 }
 
-void print_output(String name, String where, [String? vehicle]) {
+void print_output(String name, String where, [String? vehicle]) { /* [String? vehicle] la tham so tuy chon*/
   return print("$name go to $where by $vehicle");
 }
 
-void print_madinh([String? vehicle = "Bike", String thamsomacdinh = "x"]){
-  return print("Tham so mac dinh la: \[ $vehicle, $thamsomacdinh\]");
+void print_madinh([String? vehicle = "Bike", String giatrimacdinh = "x"]){ /* Bike va x la  gia tri mac dinh*/
+  return print("Tham so mac dinh la: \[ $vehicle, $giatrimacdinh\]");
+  /* Gia tri mac dinh chi khai bao dung cu phap trong tham so tuy chon */
 }
 
